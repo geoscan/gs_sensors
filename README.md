0877ba586c4cddda5174c852683ae38b3518d7f0
IlyaDanilenko <ilya@eadsoft.com>
2020-10-16 18:23:38 +0400

    update to 1.1.0

--- README.md
+++ README.md
@@ -20,33 +20,28 @@
 
 #### Поля:
 * __alive - rospy.ServiceProxy: gs_interfaces.srv.Live
-* __lpspos_service - rospy.ServiceProxy: gs_interfaces.srv.LpsPos
 * __lpsvel_service - rospy.ServiceProxy: gs_interfaces.srv.LpsVel
 * __lpsyaw_service - rospy.ServiceProxy: gs_interfaces.srv.LpsYaw
 * __gyro_service - rospy.ServiceProxy: gs_interfaces.srv.Gyro
 * __accel_service - rospy.ServiceProxy: gs_interfaces.srv.Accel
 * __orientation_service - rospy.ServiceProxy: gs_interfaces.srv.Orientation
-* __range_service - rospy.ServiceProxy: gs_interfaces.srv.Range
 * __altitude_service - rospy.ServiceProxy: gs_interfaces.srv.Altitude
 
 #### Методы:
-* lpsPosition - возвращает позицию в LPS координатах (x,y,z)
 * lpsVelocity - возвращает скорость коптера возвращаемую LPS (vx,vy,vz)
 * lpsYaw - возвращает угол поворота в системе LPS
 * gyro - возвращает данные c гироскопа (gx,gy,gz)
 * accel -  возвращает данные c акселерометра (ax,ay,az)
 * orientation - возвращает данные положения (roll,pitch,azimuth)
-* range - возвращает данные c датчиков расстояния
 * altitude - возвращает данные высоты по барометру
 
 #### Используемые сервисы:
-* geoscan/sensors/lpspos_service (gs_interfaces/LpsPos)
+* geoscan/alive (gs_interfaces/Live)
 * geoscan/sensors/lpsvel_service (gs_interfaces/LpsVel)
 * geoscan/sensors/lpsyaw_service (gs_interfaces/LpsYaw)
 * geoscan/sensors/gyro_service (gs_interfaces/Gyro)
 * geoscan/sensors/accel_service (gs_interfaces/Accel)
 * geoscan/sensors/orientation_service (gs_interfaces/Orientation)
-* geoscan/sensors/range_service (gs_interfaces/Range)
 * geoscan/sensors/altitude_service (gs_interfaces/Altitude)
 
 ## Описание нод:
@@ -66,7 +61,6 @@ ROS:
 * gs_interfaces
 * gs_core
 * std_msgs
-* geometry_msgs
 
 ## Примечание:
 Все классы в данном пакете могут быть использованы только при запущеной ноде ros_serial_node.py из пакета gs_core
--- gs_sensors/core.py
+++ gs_sensors/core.py
@@ -2,45 +2,32 @@
 # -*- coding: utf-8 -*-
 
 import rospy
-from gs_interfaces.srv import LpsPos,LpsVel,LpsYaw,Gyro,Accel,Orientation,Live,Range,Altitude
+from gs_interfaces.srv import LpsVel,LpsYaw,Gyro,Accel,Orientation,Live,Altitude
 from gs_interfaces.msg import SimpleBatteryState
-from geometry_msgs.msg import Point
 from rospy import ServiceProxy,Subscriber
 from std_msgs.msg import Bool
 
 class SensorManager():
-    def __callback(self,data):
+    def __power_callback(self,data):
         self.__battery_state=data
 
     def __init__(self):
         self.error_number=-255.
+        self.__battery_state=SimpleBatteryState()
         rospy.wait_for_service("geoscan/alive")
-        rospy.wait_for_service("geoscan/sensors/lpspos_service")
         rospy.wait_for_service("geoscan/sensors/lpsvel_service")
         rospy.wait_for_service("geoscan/sensors/lpsyaw_service")
         rospy.wait_for_service("geoscan/sensors/gyro_service")
         rospy.wait_for_service("geoscan/sensors/accel_service")
         rospy.wait_for_service("geoscan/sensors/orientation_service")
-        rospy.wait_for_service("geoscan/sensors/range_service")
         self.__alive=ServiceProxy("geoscan/alive",Live)
-        self.__lpspos_service=ServiceProxy("geoscan/sensors/lpspos_service",LpsPos)
         self.__lpsvel_service=ServiceProxy("geoscan/sensors/lpsvel_service",LpsVel)
         self.__lpsyaw_service=ServiceProxy("geoscan/sensors/lpsyaw_service",LpsYaw)
         self.__gyro_service=ServiceProxy("geoscan/sensors/gyro_service",Gyro)
         self.__accel_service=ServiceProxy("geoscan/sensors/accel_service",Accel)
         self.__orientation_service=ServiceProxy("geoscan/sensors/orientation_service",Orientation)
-        self.__range_service=ServiceProxy("geoscan/sensors/range_service",Range)
         self.__altitude_service=ServiceProxy("geoscan/sensors/altitude_service",Altitude)
-        self.__battery_state=SimpleBatteryState()
-        self.__power_sub=Subscriber("geoscan/battery_state",SimpleBatteryState,self.__callback)
-
-    def lpsPosition(self):
-        if(self.__alive().status):
-            otv=self.__lpspos_service()
-            return otv.lps_position.x,otv.lps_position.y,otv.lps_position.z
-        else:
-            rospy.loginfo("Wait, connecting to flight controller")
-            return self.error_number,self.error_number,self.error_number
+        self.__power_sub=Subscriber("geoscan/battery_state",SimpleBatteryState,self.__power_callback)
 
     def lpsVelocity(self):
         if(self.__alive().status):
@@ -88,13 +75,5 @@ class SensorManager():
             rospy.loginfo("Wait, connecting to flight controller")
             return self.error_number       
 
-    def range(self):
-        if(self.__alive().status):
-            otv=self.__range_service()
-            return otv.distance1,otv.distance2,otv.distance3,otv.distance4,otv.distance5
-        else:
-            rospy.loginfo("Wait, connecting to flight controller")
-            return self.error_number,self.error_number,self.error_number,self.error_number,self.error_number
-
     def power(self):
-        return self.__battery_state.current,self.__battery_state.charge,self.__battery_state.secs
+        return self.__battery_state.current,self.__battery_state.charge,self.__battery_state.header.stamp.secs
