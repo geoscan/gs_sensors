55c5a94900ac7fce69139adcd30e728421142376
IlyaDanilenko <ilya@eadsoft.com>
2020-12-11 12:06:38 +0400

    Update to 1.2.0

--- gs_sensors/core.py
+++ gs_sensors/core.py
@@ -2,78 +2,92 @@
 # -*- coding: utf-8 -*-
 
 import rospy
-from gs_interfaces.srv import LpsVel,LpsYaw,Gyro,Accel,Orientation,Live,Altitude
-from gs_interfaces.msg import SimpleBatteryState
+from gs_interfaces.srv import Live
+from gs_interfaces.msg import SimpleBatteryState,Orientation
 from rospy import ServiceProxy,Subscriber
-from std_msgs.msg import Bool
+from std_msgs.msg import Bool,Float32
+from geometry_msgs.msg import Point
+
 
 class SensorManager():
     def __power_callback(self,data):
-        self.__battery_state=data
+        self.__battery_state = data
+
+    def __gyro_callback(self,data):
+        self.__gyro = data
+    
+    def __accel_callback(self,data):
+        self.__accel = data
+
+    def __orientation_callback(self,data):
+        self.__orientation = data
+
+    def __altitude_callback(self,data):
+        self.__altitude = data.data
+
+    def __mag_callback(self,data):
+        self.__mag = data
 
     def __init__(self):
-        self.error_number=-255.
-        self.__battery_state=SimpleBatteryState()
+        self.error_number = -255.
+        self.__battery_state = SimpleBatteryState()
+        self.__gyro = Point()
+        self.__accel = Point()
+        self.__orientation = Orientation()
+        self.__altitude = 0.0
+        self.__mag = Point()
         rospy.wait_for_service("geoscan/alive")
-        rospy.wait_for_service("geoscan/sensors/lpsvel_service")
-        rospy.wait_for_service("geoscan/sensors/lpsyaw_service")
-        rospy.wait_for_service("geoscan/sensors/gyro_service")
-        rospy.wait_for_service("geoscan/sensors/accel_service")
-        rospy.wait_for_service("geoscan/sensors/orientation_service")
-        self.__alive=ServiceProxy("geoscan/alive",Live)
-        self.__lpsvel_service=ServiceProxy("geoscan/sensors/lpsvel_service",LpsVel)
-        self.__lpsyaw_service=ServiceProxy("geoscan/sensors/lpsyaw_service",LpsYaw)
-        self.__gyro_service=ServiceProxy("geoscan/sensors/gyro_service",Gyro)
-        self.__accel_service=ServiceProxy("geoscan/sensors/accel_service",Accel)
-        self.__orientation_service=ServiceProxy("geoscan/sensors/orientation_service",Orientation)
-        self.__altitude_service=ServiceProxy("geoscan/sensors/altitude_service",Altitude)
-        self.__power_sub=Subscriber("geoscan/battery_state",SimpleBatteryState,self.__power_callback)
-
-    def lpsVelocity(self):
-        if(self.__alive().status):
-            otv=self.__lpsvel_service()
-            return otv.lps_velocity.x,otv.lps_velocity.y,otv.lps_velocity.z
-        else:
-            rospy.loginfo("Wait, connecting to flight controller")
-            return self.error_number,self.error_number,self.error_number
-        
-    def lpsYaw(self):
-        if(self.__alive().status):
-            return self.__lpsyaw_service().angle
-        else:
-            rospy.loginfo("Wait, connecting to flight controller")
-            return self.error_number
+        self.__alive = ServiceProxy("geoscan/alive",Live)
+        self.__gyro_subscriber = Subscriber("geoscan/sensors/gyro",Point,self.__gyro_callback)
+        self.__accel_subscriber = Subscriber("geoscan/sensors/accel",Point,self.__accel_callback)
+        self.__orientation_subscriber = Subscriber("geoscan/sensors/orientation",Orientation,self.__orientation_callback)
+        self.__altitude_subscriber = Subscriber("geoscan/sensors/altitude",Float32,self.__altitude_callback)
+        self.__mag_subscriber = Subscriber("geoscan/sensors/mag",Point,self.__mag_callback)
+        self.__power_subscriber = Subscriber("geoscan/battery_state",SimpleBatteryState,self.__power_callback)
+        rospy.wait_for_message("geoscan/sensors/gyro",Point)
+        rospy.wait_for_message("geoscan/sensors/accel",Point)
+        rospy.wait_for_message("geoscan/sensors/orientation",Orientation)
+        rospy.wait_for_message("geoscan/sensors/altitude",Float32)
+        rospy.wait_for_message("geoscan/battery_state",SimpleBatteryState)
 
     def gyro(self):
-        if(self.__alive().status):
-            otv=self.__gyro_service()
-            return otv.gyro.x,otv.gyro.y,otv.gyro.z
+        if self.__alive().status:
+            return self.__gyro.x,self.__gyro.y,self.__gyro.z
         else:
-            rospy.loginfo("Wait, connecting to flight controller")
+            rospy.logwarn("Wait, connecting to flight controller")
             return self.error_number,self.error_number,self.error_number
 
     def accel(self):
-        if(self.__alive().status):
-            otv=self.__accel_service()
-            return otv.accel.x,otv.accel.y,otv.accel.z
+        if self.__alive().status:
+            return self.__accel.x,self.__accel.y,self.__accel.z
         else:
-            rospy.loginfo("Wait, connecting to flight controller")
+            rospy.logwarn("Wait, connecting to flight controller")
             return self.error_number,self.error_number,self.error_number
 
     def orientation(self):
-        if(self.__alive().status):
-            otv=self.__orientation_service()
-            return otv.roll,otv.pitch,otv.azimuth
+        if self.__alive().status:
+            return self.__orientation.roll,self.__orientation.pitch,self.__orientation.azimuth
         else:
-            rospy.loginfo("Wait, connecting to flight controller")
+            rospy.logwarn("Wait, connecting to flight controller")
             return self.error_number,self.error_number,self.error_number
 
     def altitude(self):
-        if(self.__alive().status):
-            return self.__altitude_service().altitude
+        if self.__alive().status:
+            return self.__altitude
         else:
-            rospy.loginfo("Wait, connecting to flight controller")
+            rospy.logwarn("Wait, connecting to flight controller")
             return self.error_number       
 
+    def mag(self):
+        if self.__alive().status:
+            return self.__mag.x,self.__mag.y,self.__mag.z
+        else:
+            rospy.logwarn("Wait, connecting to flight controller")
+            return self.error_number,self.error_number,self.error_number
+
     def power(self):
-        return self.__battery_state.current,self.__battery_state.charge,self.__battery_state.header.stamp.secs
+        if self.__alive().status:
+            return self.__battery_state.charge,self.__battery_state.header.stamp.secs
+        else:
+            rospy.logwarn("Wait, connecting to flight controller")
+            return self.error_number,self.error_number
