51d20c64973dd6787ac32a35d7b74efcef27e6b6
Ilya Danilenko <47718218+IlyaDanilenko@users.noreply.github.com>
2021-09-27 20:02:17 +0300

    Transform ro PiZero

--- gs_sensors/core.py
+++ gs_sensors/core.py
@@ -3,9 +3,9 @@
 
 import rospy
 from gs_interfaces.srv import Live
-from gs_interfaces.msg import SimpleBatteryState,Orientation
-from rospy import ServiceProxy,Subscriber
-from std_msgs.msg import Bool,Float32
+from gs_interfaces.msg import SimpleBatteryState, Orientation
+from rospy import ServiceProxy, Subscriber
+from std_msgs.msg import Float32
 from geometry_msgs.msg import Point
 
 
@@ -25,30 +25,27 @@ class SensorManager():
     def __altitude_callback(self,data):
         self.__altitude = data.data
 
-    def __mag_callback(self,data):
-        self.__mag = data
-
-    def __init__(self):
+    def __init__(self, namespace = ""):
+        if namespace != "":
+            namespace += "/"
         self.error_number = -255.
         self.__battery_state = SimpleBatteryState()
         self.__gyro = Point()
         self.__accel = Point()
         self.__orientation = Orientation()
         self.__altitude = 0.0
-        self.__mag = Point()
-        rospy.wait_for_service("geoscan/alive")
-        self.__alive = ServiceProxy("geoscan/alive",Live)
-        self.__gyro_subscriber = Subscriber("geoscan/sensors/gyro",Point,self.__gyro_callback)
-        self.__accel_subscriber = Subscriber("geoscan/sensors/accel",Point,self.__accel_callback)
-        self.__orientation_subscriber = Subscriber("geoscan/sensors/orientation",Orientation,self.__orientation_callback)
-        self.__altitude_subscriber = Subscriber("geoscan/sensors/altitude",Float32,self.__altitude_callback)
-        self.__mag_subscriber = Subscriber("geoscan/sensors/mag",Point,self.__mag_callback)
-        self.__power_subscriber = Subscriber("geoscan/battery_state",SimpleBatteryState,self.__power_callback)
-        rospy.wait_for_message("geoscan/sensors/gyro",Point)
-        rospy.wait_for_message("geoscan/sensors/accel",Point)
-        rospy.wait_for_message("geoscan/sensors/orientation",Orientation)
-        rospy.wait_for_message("geoscan/sensors/altitude",Float32)
-        rospy.wait_for_message("geoscan/battery_state",SimpleBatteryState)
+        rospy.wait_for_service(f"{namespace}geoscan/alive")
+        self.__alive = ServiceProxy(f"{namespace}geoscan/alive", Live)
+        self.__gyro_subscriber = Subscriber(f"{namespace}geoscan/sensors/gyro", Point, self.__gyro_callback)
+        self.__accel_subscriber = Subscriber(f"{namespace}geoscan/sensors/accel", Point, self.__accel_callback)
+        self.__orientation_subscriber = Subscriber(f"{namespace}geoscan/sensors/orientation", Orientation, self.__orientation_callback)
+        self.__altitude_subscriber = Subscriber(f"{namespace}geoscan/sensors/altitude", Float32, self.__altitude_callback)
+        self.__power_subscriber = Subscriber(f"{namespace}geoscan/battery_state", SimpleBatteryState, self.__power_callback)
+        rospy.wait_for_message(f"{namespace}geoscan/sensors/gyro", Point)
+        rospy.wait_for_message(f"{namespace}geoscan/sensors/accel", Point)
+        rospy.wait_for_message(f"{namespace}geoscan/sensors/orientation", Orientation)
+        rospy.wait_for_message(f"{namespace}geoscan/sensors/altitude", Float32)
+        rospy.wait_for_message(f"{namespace}geoscan/battery_state", SimpleBatteryState)
 
     def gyro(self):
         if self.__alive().status:
@@ -77,13 +74,6 @@ class SensorManager():
         else:
             rospy.logwarn("Wait, connecting to flight controller")
             return self.error_number       
-
-    def mag(self):
-        if self.__alive().status:
-            return self.__mag.x,self.__mag.y,self.__mag.z
-        else:
-            rospy.logwarn("Wait, connecting to flight controller")
-            return self.error_number,self.error_number,self.error_number
 
     def power(self):
         if self.__alive().status:
