5d728b7678e6daf1babf0abbfe5fdbad62a3232d
IlyaDanilenko <ilya@eadsoft.com>
2022-02-10 14:35:11 +0300

    Beautiful code 	modified:   CMakeLists.txt 	modified:   LICENSE 	modified:   README.md 	modified:   gs_sensors/core.py 	modified:   package.xml 	modified:   setup.py 	modified:   src/ultrasonic_node.py

--- gs_sensors/core.py
+++ gs_sensors/core.py
@@ -3,29 +3,29 @@
 
 import rospy
 from gs_interfaces.srv import Live
-from gs_interfaces.msg import SimpleBatteryState,Orientation
-from rospy import ServiceProxy,Subscriber
-from std_msgs.msg import Bool,Float32
+from gs_interfaces.msg import SimpleBatteryState, Orientation
+from rospy import ServiceProxy, Subscriber
+from std_msgs.msg import Bool, Float32
 from geometry_msgs.msg import Point
 
 
 class SensorManager():
-    def __power_callback(self,data):
+    def __power_callback(self, data):
         self.__battery_state = data
 
-    def __gyro_callback(self,data):
+    def __gyro_callback(self, data):
         self.__gyro = data
     
-    def __accel_callback(self,data):
+    def __accel_callback(self, data):
         self.__accel = data
 
-    def __orientation_callback(self,data):
+    def __orientation_callback(self, data):
         self.__orientation = data
 
-    def __altitude_callback(self,data):
+    def __altitude_callback(self, data):
         self.__altitude = data.data
 
-    def __mag_callback(self,data):
+    def __mag_callback(self, data):
         self.__mag = data
 
     def __init__(self):
@@ -37,39 +37,39 @@ class SensorManager():
         self.__altitude = 0.0
         self.__mag = Point()
         rospy.wait_for_service("geoscan/alive")
-        self.__alive = ServiceProxy("geoscan/alive",Live)
-        self.__gyro_subscriber = Subscriber("geoscan/sensors/gyro",Point,self.__gyro_callback)
-        self.__accel_subscriber = Subscriber("geoscan/sensors/accel",Point,self.__accel_callback)
-        self.__orientation_subscriber = Subscriber("geoscan/sensors/orientation",Orientation,self.__orientation_callback)
-        self.__altitude_subscriber = Subscriber("geoscan/sensors/altitude",Float32,self.__altitude_callback)
-        self.__mag_subscriber = Subscriber("geoscan/sensors/mag",Point,self.__mag_callback)
-        self.__power_subscriber = Subscriber("geoscan/battery_state",SimpleBatteryState,self.__power_callback)
-        rospy.wait_for_message("geoscan/sensors/gyro",Point)
-        rospy.wait_for_message("geoscan/sensors/accel",Point)
-        rospy.wait_for_message("geoscan/sensors/orientation",Orientation)
-        rospy.wait_for_message("geoscan/sensors/altitude",Float32)
-        rospy.wait_for_message("geoscan/battery_state",SimpleBatteryState)
+        self.__alive = ServiceProxy("geoscan/alive", Live)
+        self.__gyro_subscriber = Subscriber("geoscan/sensors/gyro", Point, self.__gyro_callback)
+        self.__accel_subscriber = Subscriber("geoscan/sensors/accel", Point, self.__accel_callback)
+        self.__orientation_subscriber = Subscriber("geoscan/sensors/orientation", Orientation, self.__orientation_callback)
+        self.__altitude_subscriber = Subscriber("geoscan/sensors/altitude", Float32, self.__altitude_callback)
+        self.__mag_subscriber = Subscriber("geoscan/sensors/mag", Point, self.__mag_callback)
+        self.__power_subscriber = Subscriber("geoscan/battery_state", SimpleBatteryState, self.__power_callback)
+        rospy.wait_for_message("geoscan/sensors/gyro", Point)
+        rospy.wait_for_message("geoscan/sensors/accel", Point)
+        rospy.wait_for_message("geoscan/sensors/orientation", Orientation)
+        rospy.wait_for_message("geoscan/sensors/altitude", Float32)
+        rospy.wait_for_message("geoscan/battery_state", SimpleBatteryState)
 
     def gyro(self):
         if self.__alive().status:
-            return self.__gyro.x,self.__gyro.y,self.__gyro.z
+            return self.__gyro.x, self.__gyro.y, self.__gyro.z
         else:
             rospy.logwarn("Wait, connecting to flight controller")
-            return self.error_number,self.error_number,self.error_number
+            return self.error_number, self.error_number, self.error_number
 
     def accel(self):
         if self.__alive().status:
-            return self.__accel.x,self.__accel.y,self.__accel.z
+            return self.__accel.x, self.__accel.y, self.__accel.z
         else:
             rospy.logwarn("Wait, connecting to flight controller")
-            return self.error_number,self.error_number,self.error_number
+            return self.error_number, self.error_number, self.error_number
 
     def orientation(self):
         if self.__alive().status:
-            return self.__orientation.roll,self.__orientation.pitch,self.__orientation.azimuth
+            return self.__orientation.roll, self.__orientation.pitch, self.__orientation.azimuth
         else:
             rospy.logwarn("Wait, connecting to flight controller")
-            return self.error_number,self.error_number,self.error_number
+            return self.error_number, self.error_number, self.error_number
 
     def altitude(self):
         if self.__alive().status:
@@ -80,14 +80,14 @@ class SensorManager():
 
     def mag(self):
         if self.__alive().status:
-            return self.__mag.x,self.__mag.y,self.__mag.z
+            return self.__mag.x, self.__mag.y, self.__mag.z
         else:
             rospy.logwarn("Wait, connecting to flight controller")
-            return self.error_number,self.error_number,self.error_number
+            return self.error_number, self.error_number, self.error_number
 
     def power(self):
         if self.__alive().status:
-            return self.__battery_state.charge,self.__battery_state.header.stamp.secs
+            return self.__battery_state.charge, self.__battery_state.header.stamp.secs
         else:
             rospy.logwarn("Wait, connecting to flight controller")
-            return self.error_number,self.error_number
+            return self.error_number, self.error_number
--- src/ultrasonic_node.py
+++ src/ultrasonic_node.py
@@ -5,68 +5,65 @@ import rospy
 from rospy import Publisher
 from std_msgs.msg import Float32
 import RPi.GPIO as GPIO
-from time import sleep
+from rospy import sleep
 from time import time
 
 class Ultrasonic:
-    def __init__(self,trig,echo,err):
-        self.TRIG=trig
-        self.ECHO=echo
-        self.sleep_time=0.0051
-        self.err=err
+    def __init__(self, trig, echo, err):
+        self.TRIG = trig
+        self.ECHO = echo
+        self.sleep_time = 0.0051
+        self.err = err
         
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
         
-        GPIO.setup(self.TRIG,GPIO.OUT)
-        GPIO.setup(self.ECHO,GPIO.IN)
+        GPIO.setup(self.TRIG, GPIO.OUT)
+        GPIO.setup(self.ECHO, GPIO.IN)
         
     def getDistance(self):
         try:
-            GPIO.output(self.TRIG,False)
+            GPIO.output(self.TRIG, False)
             sleep(0.0001)
-            GPIO.output(self.TRIG,True)
+            GPIO.output(self.TRIG, True)
             sleep(0.005)
-            GPIO.output(self.TRIG,False)
-            i=0
+            GPIO.output(self.TRIG, False)
+            i=  0
             while GPIO.input(self.ECHO) == 0:
-                start = time()
-                i+=1
-                if (i>self.err):
+                start = rospy.Time().to_sec()
+                i += 1
+                if i > self.err:
                     return float("inf")
                 
-            i=0
-            while (GPIO.input(self.ECHO) == 1):
-                end = time()
-                i+=1
-                if (i>self.err):
+            i = 0
+            while GPIO.input(self.ECHO) == 1:
+                end = rospy.Time().to_sec()
+                i += 1
+                if i > self.err:
                     return float("inf")
-                
-            duration=end-start
 
-            distance=duration*17150
-
-            distance=round(distance,2)
+            distance = round((end - start) * 17150, 2)
             
             return distance
         except:
             return float("inf")
 
-rospy.init_node("ultrasonic_node")
+if __name__ == "__main__":
+    rospy.init_node("ultrasonic_node")
 
-echo_p=rospy.search_param("echo")
-echo=int(rospy.get_param(echo_p))
-trig_p=rospy.search_param("trig")
-trig=int(rospy.get_param(trig_p))
-try:
-    err_p=rospy.search_param("err")
-    err=int(rospy.get_param(err_p))
-except:
-    err=10000
+    echo_port = int(rospy.get_param(rospy.search_param("echo")))
+    trig_port = int(rospy.get_param(rospy.search_param("trig")))
+    try:
+        err = int(rospy.get_param(rospy.search_param("err")))
+    except:
+        err = 10000
 
-pub=Publisher("ultrasonic_sensor/trig_"+str(trig)+"_echo_"+str(echo),Float32,queue_size=10)
-ultrasonic=Ultrasonic(trig,echo,err)
+    ultrasonic_publisher = Publisher(f"ultrasonic_sensor/trig_{trig_port}_echo_{echo_port}", Float32, queue_size = 10)
+    ultrasonic = Ultrasonic(trig_port, echo_port, err)
 
-while not rospy.is_shutdown():
-    distance=ultrasonic.getDistance()
-    pub.publish(Float32(distance))
+    rate = rospy.Rate(100)
+
+    while not rospy.is_shutdown():
+        distance = ultrasonic.getDistance()
+        ultrasonic_publisher.publish(Float32(distance))
+        rate.sleep()
